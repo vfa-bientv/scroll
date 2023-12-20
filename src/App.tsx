4f046b328c60a29a02c5e1f8cb186866943bca47
vfa-bientv <103976711+vfa-bientv@users.noreply.github.com>
2023-12-21 01:17:53 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import "./App.css";
+import { HorizontalScrollCarousel } from "./HorizontalScrollCarousel";
+
+function App() {
+  return (
+    <>
+      <div className="bg-neutral-800">
+        <div className="flex h-48 items-center justify-center">
+          <span className="font-semibold uppercase text-neutral-500">
+            Scroll down
+          </span>
+        </div>
+        <HorizontalScrollCarousel />
+        <div className="flex h-48 items-center justify-center">
+          <span className="font-semibold uppercase text-neutral-500">
+            Scroll up
+          </span>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
